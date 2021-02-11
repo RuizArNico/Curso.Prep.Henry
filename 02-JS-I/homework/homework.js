99a427d5091d7897c512655605905e031423eda3
Nicolas Ruiz <nruiz@NB-Nicolas-Ruiz.local>
2021-02-11 18:42:51 -0300

    97% homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -168,6 +168,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  str= str + '!';
+  return str;
   
 }
 
@@ -226,6 +228,7 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
+  
 }
 
 
